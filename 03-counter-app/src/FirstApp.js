af522f65a803d6a3ab79aa5a75d16b045ada4fb6
Ronaldo Trejo <ronaldo.trejo@kiwiik.com>
2021-12-19 15:43:58 -0600

    Add variables in JSX

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -2,11 +2,17 @@ import React, { Fragment } from "react";
 
 // My first functional component.
 
+const name = "John";
+
 const FirstApp = () => {
 
     return (
         <Fragment>
-            <h1>Hello World!</h1>
+            <h1>Hello { name } </h1>
+            { /** You can add expressions or variables that are strings or numbers, also if you add
+             * an array, it will be rendered as a list with /n as glue, the array also contains same data types.
+             * Other objects are not supported. 
+             * */ }
             <p>This is my first React app.</p>
         </Fragment>
     );
